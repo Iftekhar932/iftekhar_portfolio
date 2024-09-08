aa21280541d28ec1950e9fa2f13b617ed09a62e8
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-09-08 23:16:04 +0600

    navbar mobile size added

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-window.addEventListener("scroll", function () {
+const navCrossButton = document.getElementById("cross-button");
+
+navCrossButton.addEventListener("click", (e) => {
+  // e.target.innerHTML = `<span id="cross-button" class="cross-button">&#9776;</span>`; // need to remove previous one
+
+  e.target.textContent == "☰"
+    ? (e.target.textContent = "X")
+    : (e.target.textContent = "☰");
+});
+
+/* window.addEventListener("scroll", function () {
   console.log(document.documentElement.scrollHeight - window.innerHeight); // whole document's height - device window height
   console.log(window.scrollY); // scrolled pixel
+});
+ */
+
+const elementToDoStuff = document.querySelector("random-element");
+
+/* When al element is  visible(partially or fully based on "threshold" setting below) in the screen is intersected by Observer */
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("className", entry.isIntersecting); // "entry.isIntersecting" can be true or false, depending on in value that specified class will be added
+      if (entry.isIntersecting) observer.unobserve(entry.target); // this line is to make sure only ones an element is observed, which intersected only ones.(If needed)
+    });
+  },
+  {
+    threshold: 1, // "1" means the whole element(100%) has to be visible, 0  means the least amount of the element, 0.5 means 50%. NOTE: default is 0
+    //rootMargin: "-100px",
+    /*
+    About "rootMargin"  ⬇️
+  the code above  making the whole container -100px smaller, it's like having the window shortened,
+     events regarding window size will be triggered -100px earlier, 
+     if the "-" is off it'll enlarge the window,(example use case: pre-loading images before scrolling to the point where it should be visible) */
+  }
+);
+
+elementToDoStuff.forEach((element) => {
+  observer.observe(element[0]); //  using observe method to observe the element
 });
