e03b42db85e198676bca118889b1bdfd7e0739dd
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-09-13 19:19:19 +0600

    animation done for spans

--- index.js
+++ index.js
@@ -43,15 +43,21 @@ const navBarObserver = new IntersectionObserver(
 );
 navBarObserver.observe(navbar);
 
-/*  projects section background animation*/
-// sorting the array elements in random order
 
+/******  projects section background animation ******/
+// colors name array
 const randomColors = [
   "red",
-  "blue",
   "green",
   "yellow",
   "orange",
+  "aliceBlue",
+  "aquaMarine",
+  "aqua",
+  "peachPuff",
+  "mediumSlateBlue",
+  "hotPink",
+  "deepPink",
   "cyan",
   "lightblue",
   "pink",
@@ -68,29 +74,55 @@ const randomColors = [
   "magenta",
   "mintCream",
 ];
+
+// sorting the array elements in random order
 randomColors.sort(() => Math.random() - 0.5);
 
+// generating span tags with necessary attributes (more css available in css file for these span tags with the class name)
 for (let i = 0; i <= randomColors.length; i++) {
   const span = document.createElement("span");
+  span.style.background = `${randomColors[i]}`;
+  span.style.boxShadow = `0px 0px 10px 1px ${randomColors[i]}`;
+
   span.id = "colorSpans";
   span.classList.toggle("colorFulLines", i % 2 == 1);
   span.classList.toggle("colorRight", i % 2 == 0);
-  span.style.background = `${randomColors[i]}`;
-  span.style.boxShadow = `0px 0px 80px 44px ${randomColors[i]}`;
+
   // Add a random initial position
   span.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
   span.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+
   document.body.append(span);
 
+  // generates random initial position for each color after interval
   setInterval(() => {
     span.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
     span.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
-  }, 2000);
+  }, 1200);
+}
 
-  const colorSpans = document.querySelectorAll("#colorSpans");
 
+// colorful span tags positioning along with scroll
+const colorSpans = [...document.querySelectorAll("#colorSpans")];
+if (colorSpans[0]) {
   window.addEventListener("scroll", () => {
+    console.log(
+      document.body.scrollWidth,
+      colorSpans[0].getBoundingClientRect().y
+    );
+
+    // if span tags scrollY position is greater than the window scrollX position or scrollY position then change the tag's position
     for (const singleSpan of colorSpans) {
+      if (singleSpan.getBoundingClientRect().y > window.scrollX || window.scrollY) {
+        singleSpan.style.top = `${Math.max(
+          300,
+          Math.floor(Math.random() * window.innerHeight)
+        )}px`;
+        singleSpan.style.left = `${Math.max(
+          300,
+          Math.floor(Math.random() * window.innerWidth)
+        )}px`;
+      }
     }
   });
 }
